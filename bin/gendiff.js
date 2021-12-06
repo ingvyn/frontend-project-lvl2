6b06c4f7ad2e5ca1e7c39df87176a265377e69e7
Igor Nick <i.nikitin@eco-vector.com>
2021-12-06 18:25:44 +0300

    removed unnecessary option definings

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,14 +5,7 @@ program
     .name('gendiff')
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output usage information')
     .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {});
+    .arguments('<filepath1> <filepath2>');
 
 program.parse();
-const options = program.opts();
-if (options.help) {
-    console.log(program.help());
-}
