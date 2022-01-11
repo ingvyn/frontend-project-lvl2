d25c668e1f838649f4d85466bc9f3563eb234b42
Igor Nick <i.nikitin@eco-vector.com>
2022-01-11 14:00:51 +0300

    setup eslint and fix linting errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
 import makeDiff from '../src/make-diff.js';
+
 const program = new Command();
 
 program
-    .name('gendiff')
-    .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-        console.log(makeDiff(filepath1, filepath2));
-    });
+  .name('gendiff')
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(makeDiff(filepath1, filepath2));
+  });
 
 program.parse();
--- src/make-diff.js
+++ src/make-diff.js
@@ -3,27 +3,22 @@ import { resolve } from 'path';
 import { cwd } from 'process';
 import _ from 'lodash';
 
-const isPropertyRemain = ([prop, value], object, result) => {
-    if (_.has(object, prop)) {
-
-    }
-}
 export default (filepath1, filepath2) => {
-    const path1 = resolve(cwd(), filepath1);
-    const path2 = resolve(cwd(), filepath2);
-    const obj1 = JSON.parse(fs.readFileSync(filepath1, 'utf8'));
-    const obj2 = JSON.parse(fs.readFileSync(filepath2, 'utf8'));
-    const handleKeyString = (key) => {
-        if (_.has(obj1, key)) {
-            if (!_.has(obj2, key)) {
-                return `  - ${key}: ${obj2[key]}\n`;
-            }
-            if (obj1[key] === obj2[key]) return `    ${key}: ${obj1[key]}\n`;
-            return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}\n`;
-        }
-        return `  + ${key}: ${obj2[key]}\n`;
-    };
-    const allObjectKeys = _.sortBy(Object.keys({...obj1, ...obj2}));
-    const resString = '{\n' + allObjectKeys.reduce((acc, key) => acc + handleKeyString(key), '') + '}';
-    return resString;
+  const path1 = resolve(cwd(), filepath1);
+  const path2 = resolve(cwd(), filepath2);
+  const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
+  const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
+  const handleKeyString = (key) => {
+    if (_.has(obj1, key)) {
+      if (!_.has(obj2, key)) {
+        return `  - ${key}: ${obj2[key]}\n`;
+      }
+      if (obj1[key] === obj2[key]) return `    ${key}: ${obj1[key]}\n`;
+      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}\n`;
+    }
+    return `  + ${key}: ${obj2[key]}\n`;
+  };
+  const allObjectKeys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
+  const resString = `{\n${allObjectKeys.reduce((acc, key) => acc + handleKeyString(key), '')}}`;
+  return resString;
 };
