967ac36c33a50690e20e45b80beab221aca2ef6c
Igor Nick <i.nikitin@eco-vector.com>
2022-01-21 19:10:04 +0300

    add enviroment for jest testing

---
+++ __tests__/make-diff.test.js
@@ -0,0 +1,10 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import makeDiff from '../src/make-diff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+test('gendifference', () => {
+
+});
