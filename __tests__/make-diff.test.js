c3492cc2da6e8d48f5a3d4c06b238601a119288b
Igor Nick <i.nikitin@eco-vector.com>
2022-02-04 19:16:43 +0300

    set test to get fixtures

--- __tests__/make-diff.test.js
+++ __tests__/make-diff.test.js
@@ -1,10 +1,14 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+//import { fileURLToPath } from 'url';
+import path from 'path';
 import makeDiff from '../src/make-diff.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.resolve('./__fixtures__', filename);
 
 test('gendifference', () => {
-
+	const path1 = getFixturePath('file1.json');
+	const path2 = getFixturePath('file2.json');
+	expect(makeDiff(path1, path2)).toEqual(`{\n  - follow: undefined\n    host: hexlet.io
+  - proxy: undefined\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}`);
 });
