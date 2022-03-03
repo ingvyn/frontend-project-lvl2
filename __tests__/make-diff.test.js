c99fec00c18561b230912fcc4cbb0bc9c867936f
Igor Nick <i.nikitin@eco-vector.com>
2022-03-03 13:46:37 +0300

    make test get result from file

--- __tests__/make-diff.test.js
+++ __tests__/make-diff.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -10,6 +11,7 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 test('gendifference', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
-  expect(makeDiff(path1, path2)).toEqual(`{\n  - follow: undefined\n    host: hexlet.io
-  - proxy: undefined\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}`);
+  const resPath = getFixturePath('result');
+  const result = fs.readFileSync(resPath, 'utf8');
+  expect(makeDiff(path1, path2)).toEqual(result);
 });
