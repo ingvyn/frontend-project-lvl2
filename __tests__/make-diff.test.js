1bfa07e7ab0e0b791e75f44c1f51c131f6f1e8b4
Igor Nick <i.nikitin@eco-vector.com>
2022-07-12 17:37:05 +0300

    add test for different types of changes in json files

--- __tests__/make-diff.test.js
+++ __tests__/make-diff.test.js
@@ -8,10 +8,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('gendifference', () => {
-  const path1 = getFixturePath('file1.json');
-  const path2 = getFixturePath('file2.json');
-  const resPath = getFixturePath('result');
+test('delete_key', () => {
+  const path1 = getFixturePath('source.json');
+  const path2 = getFixturePath('delete_key.json');
+  const resPath = getFixturePath('expected_delete_key');
+  const result = fs.readFileSync(resPath, 'utf8');
+  expect(makeDiff(path1, path2)).toEqual(result);
+});
+
+test('change_key', () => {
+  const path1 = getFixturePath('source.json');
+  const path2 = getFixturePath('change_key.json');
+  const resPath = getFixturePath('expected_change_key');
+  const result = fs.readFileSync(resPath, 'utf8');
+  expect(makeDiff(path1, path2)).toEqual(result);
+});
+
+test('add_key', () => {
+  const path1 = getFixturePath('source.json');
+  const path2 = getFixturePath('add_key.json');
+  const resPath = getFixturePath('expected_add_key');
   const result = fs.readFileSync(resPath, 'utf8');
   expect(makeDiff(path1, path2)).toEqual(result);
 });
