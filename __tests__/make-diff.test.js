c3b48871774d7013ac6ac85dbcf6fc47e0489ac8
Igor Nick <i.nikitin@eco-vector.com>
2022-10-15 14:11:57 +0300

    add tests for yaml

--- __tests__/make-diff.test.js
+++ __tests__/make-diff.test.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe, beforeAll } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import makeDiff from '../src/make-diff.js';
@@ -7,27 +7,28 @@ import makeDiff from '../src/make-diff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-test('delete_key', () => {
-  const path1 = getFixturePath('source.json');
-  const path2 = getFixturePath('delete_key.json');
-  const resPath = getFixturePath('expected_delete_key');
+let extension;
+const testAction = (actionName) => {
+  const path1 = getFixturePath('source'.concat(extension));
+  const path2 = getFixturePath(actionName.concat(extension));
+  const resPath = getFixturePath('expected_'.concat(actionName));
   const result = fs.readFileSync(resPath, 'utf8');
   expect(makeDiff(path1, path2)).toEqual(result);
+};
+const testSuiteRun = () => {
+  test('delete_key', testAction.bind(null, 'delete_key'));
+  test('change_key', testAction.bind(null, 'change_key'));
+  test('add_key', testAction.bind(null, 'add_key'));
+};
+describe('JSON', () => {
+  beforeAll(() => {
+    extension = '.json';
+  });
+  testSuiteRun();
 });
-
-test('change_key', () => {
-  const path1 = getFixturePath('source.json');
-  const path2 = getFixturePath('change_key.json');
-  const resPath = getFixturePath('expected_change_key');
-  const result = fs.readFileSync(resPath, 'utf8');
-  expect(makeDiff(path1, path2)).toEqual(result);
-});
-
-test('add_key', () => {
-  const path1 = getFixturePath('source.json');
-  const path2 = getFixturePath('add_key.json');
-  const resPath = getFixturePath('expected_add_key');
-  const result = fs.readFileSync(resPath, 'utf8');
-  expect(makeDiff(path1, path2)).toEqual(result);
+describe('YAML', () => {
+  beforeAll(() => {
+    extension = '.yml';
+  });
+  testSuiteRun();
 });
