3ae75aa2fe7d56e7e14e4a23012fc0d077abb0fc
Igor Nick <i.nikitin@eco-vector.com>
2022-10-27 16:46:12 +0300

    fix err in src module and add fixtures for resting processing nested objects

--- __fixtures__/expected.js
+++ __fixtures__/expected.js
@@ -7,14 +7,40 @@ export default {
         1st: a
         2nd: b
     }
+    dObject: {
+        aRemain: 1
+        bChanged: 0
+        cDeleted: true
+      + eAdded: true
+    }
+    sObject: {
+        sKey: sValue
+    }
 }`,
   change_key: `{
     aDelete: true
   - bChValue: no
   + bChValue: yes
+    dObject: {
+        aRemain: 1
+      - bChanged: 0
+      + bChanged: 1
+        cDeleted: true
+    }
+    sObject: {
+        sKey: sValue
+    }
 }`,
   delete_key: `{
   - aDelete: true
     bChValue: no
+    dObject: {
+        aRemain: 1
+        bChanged: 0
+      - cDeleted: true
+    }
+  - sObject: {
+        sKey: sValue
+    }
 }`,
 };
