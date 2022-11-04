0395e3b217c9c7ee675764badd021e4de11c06c2
Igor Nick <ingvyn@yandex.ru>
2022-11-04 23:46:31 +0300

    design of inner object of diff

--- src/make-diff.js
+++ src/make-diff.js
@@ -11,44 +11,57 @@ const makeDiff = (filepath1, filepath2) => {
   const parser2 = parsers[extname(path2)];
   const object1 = parser1(fs.readFileSync(path1, 'utf8'));
   const object2 = parser2(fs.readFileSync(path2, 'utf8'));
-  const startIndent = 0;
-  const indentStep = 4;
-  const makeObjectsDiff = (obj1, obj2, formatIndent) => {
-    const baseIndent = ' '.repeat(formatIndent);
-    const plusIndent = '  + ';
-    const minusIndent = '  - ';
-    const neutralIndent = '    ';
+  const getChildren = (obj) => {
 
+  };
+  const makeObjectsDiff = (obj1, obj2) => {
     const handleKeyDiff = (key) => {
-      const indent = formatIndent + indentStep;
-      const displayKeyValue = (keyValue) => {
-        if (!(keyValue instanceof Object)) {
-          return keyValue;
-        }
-        return makeObjectsDiff(keyValue, _.cloneDeep(keyValue), indent);
+      let state;
+      let value;
+      let children;
+      const getKeyValues = (obj) => {
+        const isObject = _.isObject(obj[key]);
+        children = isObject ? getChildren(obj[key]) : [];
+        value = isObject ? obj[key] : null;
+        return { children, value };
       };
-
       if (_.has(obj1, key)) {
         if (!_.has(obj2, key)) {
-          return `${baseIndent}${minusIndent}${key}: ${displayKeyValue(obj1[key])}`;
+          state = 'deleted';
+          return {
+            key, state, ...getKeyValues(obj1),
+          };
         }
         if (obj1[key] === obj2[key]) {
-          return `${baseIndent}${neutralIndent}${key}: ${displayKeyValue(obj1[key])}`;
+          state = 'unchanged';
+          return {
+            key, state, ...getKeyValues(obj1),
+          };
         }
-        if ((obj1[key] instanceof Object) && (obj2[key] instanceof Object)) {
-          return `${baseIndent}${neutralIndent}${key}: ${makeObjectsDiff(obj1[key], obj2[key], indent)}`;
+        if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+          state = 'unchanged';
+          children = makeObjectsDiff(obj1[key], obj2[key]);
+          value = null;
+          return {
+            key, state, children, value,
+          };
         }
-        return `${baseIndent}${minusIndent}${key}: ${displayKeyValue(obj1[key])}\n${baseIndent}${plusIndent}${key}: ${displayKeyValue(obj2[key])}`;
+        state = 'changed';
+        return {
+          key, state, initial: { ...getKeyValues(obj1) }, ...getKeyValues(obj2),
+        };
       }
-      return `${baseIndent}${plusIndent}${key}: ${displayKeyValue(obj2[key])}`;
+      state = 'added';
+      return {
+        key, state, ...getKeyValues(obj2),
+      };
     };
 
     const allObjectsKeys = _.sortBy(Object.keys({ ...obj1, ...obj2 }));
-    const resString = allObjectsKeys.map((key) => handleKeyDiff(key));
-    return `{\n${resString.join('\n')}\n${baseIndent}}`;
+    return allObjectsKeys.map((key) => handleKeyDiff(key));
   };
 
-  return makeObjectsDiff(object1, object2, startIndent);
+  return makeObjectsDiff(object1, object2);
 };
 
 export default makeDiff;
