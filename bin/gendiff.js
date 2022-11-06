0ee2230b13f23233beaadfe9c05686c6c6e27944
Igor Nick <i.nikitin@eco-vector.com>
2022-11-06 20:25:53 +0300

    use stylish as default format in script

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(makeDiff(filepath1, filepath2));
